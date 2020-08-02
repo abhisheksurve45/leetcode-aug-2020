f97f68465b84ce6c5bbcfc0119889d4782c2457b
Abhishek Surve <abhishek.sur@media.net>
2020-08-02 23:43:57 +0530

    W1:Q3: DesignHashSet

--- WEEK1/DesignHashSet.java
+++ WEEK1/DesignHashSet.java
@@ -38,4 +38,70 @@ public class DesignHashSet {
      * obj.remove(key);
      * boolean param_3 = obj.contains(key);
      */
+
+
+    /**
+     * Solution using Linked List (chaining)
+     */
+    class MyHashSet {
+
+        private Bucket[] buckets;
+        private int keyRange;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyHashSet() {
+            this.keyRange = 769;
+            this.buckets = new Bucket[this.keyRange];
+            for (int i = 0; i < this.keyRange; ++i) {
+                this.buckets[i] = new Bucket();
+            }
+        }
+
+        protected int _hash(int key) {
+            return key % this.keyRange;
+        }
+
+        public void add(int key) {
+            int bucketIndex = _hash(key);
+            this.buckets[bucketIndex].insert(key);
+        }
+
+        public void remove(int key) {
+            int bucketIndex = _hash(key);
+            this.buckets[bucketIndex].delete(key);
+        }
+
+        /**
+         * Returns true if this set contains the specified element
+         */
+        public boolean contains(int key) {
+            int bucketIndex = _hash(key);
+            return this.buckets[bucketIndex].exists(key);
+        }
+    }
+
+    class Bucket {
+
+        private LinkedList<Integer> container;
+
+        public Bucket() {
+            this.container = new LinkedList<Integer>();
+        }
+
+        public void insert(int key) {
+            int index = this.container.indexOf(key);
+            if (index == -1) this.container.addFirst(key);
+        }
+
+        public void delete(int key) {
+            this.container.remove(key);
+        }
+
+        public boolean exists(int key) {
+            int index = this.container.indexOf(key);
+            return index != -1;
+        }
+    }
 }

e6823c55f58f9e188ad3941510b2fd1eac427872
Abhishek Surve <abhishek.sur@media.net>
2020-08-08 16:54:15 +0530

    W2:Q1: ClosestBinarySearchTreeValue W2:Q2: PathSumIII

--- WEEK2/ClosestBinarySearchTreeValue.java
+++ WEEK2/ClosestBinarySearchTreeValue.java
@@ -1,3 +1,26 @@
 public class ClosestBinarySearchTreeValue {
 
+    /**
+     * Q : Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+     *
+     *     4
+     *    / \
+     *   2   5
+     *  / \
+     * 1   3
+     *
+     * Input : root = [4,2,5,1,3], target = 3.714286
+     *
+     * Output: 4
+     */
+    public int closestValue(TreeNode root, double target) {
+        int ret = root.val;
+        while (root != null) {
+            if (Math.abs(target - root.val) < Math.abs(target - ret)) {
+                ret = root.val;
+            }
+            root = root.val > target ? root.left : root.right;
+        }
+        return ret;
+    }
 }

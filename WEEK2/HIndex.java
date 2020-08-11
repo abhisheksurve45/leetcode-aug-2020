91b7512b2ac06cd9423cbc1a48805738111927bd
Abhishek Surve <abhishek.sur@media.net>
2020-08-11 18:19:58 +0530

    W2:Q5: HIndex

--- WEEK2/HIndex.java
+++ WEEK2/HIndex.java
@@ -1,5 +1,8 @@
 public class HIndex {
 
+    /**
+     * Sorting approach
+     */
     class Solution {
 
         public int hIndex(int[] citations) {
@@ -12,6 +15,37 @@ public class HIndex {
                 if (citations[i] >= n - i) {
                     return (n - i);
                 }
+
+            }
+
+            return 0;
+
+        }
+    }
+
+    /**
+     * Bucket approach
+     */
+    class Solution {
+
+        public int hIndex(int[] citations) {
+
+            int[] bucket = new int[citations.length + 1];
+
+            for (int i = 0; i < citations.length; i++) {
+                if (citations[i] > citations.length) {
+                    bucket[citations.length]++;
+                } else {
+                    bucket[citations[i]]++;
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = bucket.length - 1; i >= 0; i--) {
+
+                sum += bucket[i];
+                if (sum >= i) return i;
 
             }
 
